1cf710f73eb29854115aa76334171dc5df78ee46
Fernando Guirao <fernando@bueninvento.es>
2014-03-09 11:06:16 +0100

    Configurado parallax y video

--- Developer/static/js/script.js
+++ Developer/static/js/script.js
@@ -29,4 +29,46 @@ $(document).ready(function() {
     $('.content-23').each(function() {
         $(this).parallax('50%', 0.3, true);
     });
+
+    // STELLAR JS
+    $(window).stellar();
+
+// VIDEO BACKGROUND
+
+    //resize video
+    $('.video').height($(window).height());
+    $(window).resize(function() {
+        $('.video').height($(window).height());
+    });
+    
+    //show the icon
+    $('.video .cont').addClass('visible');
+
+    //show the beauty
+    setTimeout(function() {
+        $('.video .sdf, .video .suys, .video .arrow').addClass('visible');
+    }, 2000);
+    
+    //bg video
+    $.backgroundVideo($('#bgVideo'), {
+        "align": "centerXY",
+        "path": "static/media/",
+        "videoid": "video01",
+        "width": 846,
+        "height": 476,
+        "filename": "sexy",
+        "types": ["mp4", "ogg", "webm"]
+    });
+
+    $.backgroundVideo($('#bgVideo2'), {
+        "align": "centerXY",
+        "videoid": "video02",
+         "path": "static/media/",
+        "width": 846,
+        "height": 476,
+        "filename": "sexy",
+        "types": ["mp4", "ogg", "webm"]
+    });
+    
+    // FIN DE VIDEO BACKGROUND
 });
